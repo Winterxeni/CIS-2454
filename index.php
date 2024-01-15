9c3e6ed5e6051cd99fa22613247cbc28d5031045
Winterxeni <97712382+Winterxeni@users.noreply.github.com>
2024-01-15 12:28:33 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo "<h2>Minnie Ragin / CS: Software Engineering. Last round of classes before I can transfer
+            I am currently employed at Aptiv as a Software Develop Dev.
+            Currently creating a safety database there for the company to use!
+            It's making me want to rip my hair out but it's fun.</h2>";        
+            echo "</br>My hobbies are video games, reading, writing, painting, and learning Greek.";      
+            echo "</br>Maybe try to compete in an ice skating competition or being the process of buying a home.";                  
+            echo "</br>I want to concentrate on game design, though I work in the automotive space currently.";   
+                   
+        ?>
+    </body>
+</html>
